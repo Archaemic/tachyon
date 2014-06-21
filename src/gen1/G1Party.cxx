fa4e781ed33a82d12d3d0ff39a9a772c1f09f708
Archaemic <archaemic@gmail.com>
2014-06-21 15:44:58 -0700

    Add support for Japanese parties in Generation 1

--- src/gen1/G1Party.cxx
+++ src/gen1/G1Party.cxx
@@ -3,13 +3,23 @@
 #include "gen1/Generation1.h"
 
 enum {
+	G10J_PARTY_POKEMON = 0x2ED5,
 	G10E_PARTY_POKEMON = 0x2F2C,
 };
 
 G1Party::G1Party(Generation1* gen)
 	: GBGroup<G1PartyPokemon>(gen)
 {
-	setStart(&gen->ram()[G10E_PARTY_POKEMON]);
+	switch (gen->version() & Game::MASK_LOCALIZATION) {
+	case Game::JAPANESE:
+		setStart(&gen->ram()[G10J_PARTY_POKEMON]);
+		setNameLength(6);
+		break;
+	case Game::ENGLISH:
+	default:
+		setStart(&gen->ram()[G10E_PARTY_POKEMON]);
+		break;
+	}
 }
 
 unsigned G1Party::capacity() const {
